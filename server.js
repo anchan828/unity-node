5d6cea1a540f872f383f796add417b95c449026a
kyusyukeigo <anchan828@gmail.com>
2012-07-12 22:08:03 +0900

    commit

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-var html = 'マシン名 unityは頂いた！\n';
-  html+='<a href="http://twitter.com/kyusyukeigo">@kyusyukeigo</a>'
-res.end();
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World Unity');
 }).listen(80);
