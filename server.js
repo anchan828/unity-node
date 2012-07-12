a19819d450cc122251b01bb07849417df7ee0254
kyusyukeigo <anchan828@gmail.com>
2012-07-12 22:10:35 +0900

    comit2

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World Unity');
+  response.end('Hello World Unity.');
 }).listen(80);
